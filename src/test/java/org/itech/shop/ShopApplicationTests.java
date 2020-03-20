d5a7989b1ec635174ae2895dc0b67527afe1b210
liuqiang <gavineil@outlook.com>
2020-03-20 17:26:20 +0800

    init

--- src/test/java/org/itech/shop/ShopApplicationTests.java
+++ src/test/java/org/itech/shop/ShopApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -18,7 +17,6 @@ import java.util.Optional;
 
 @SpringBootTest
 @Transactional
-@Rollback
 class ShopApplicationTests {
     @Autowired
     private UserRepository userRepository;
